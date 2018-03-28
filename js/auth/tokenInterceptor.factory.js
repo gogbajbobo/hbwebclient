a9c018ddfae3b5f128b43f7768f3c11ea8c2e0e2
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-28 16:01:49 +0300

    console.log

--- js/auth/tokenInterceptor.factory.js
+++ js/auth/tokenInterceptor.factory.js
@@ -9,12 +9,10 @@ function TokenInterceptor($injector) {
 
             if (AuthService.token) {
 
-                console.log('1', config.headers);
+                config.headers.Authorization = AuthService.token;
+                // config.headers['Content-Type'] = "application/json";
 
-                config.headers['Authorization'] = AuthService.token;
-                config.headers['Content-Type'] = "application/json";
-
-                console.log('2', config.headers);
+                console.log(config.url, config.headers);
 
             }
             return config;
